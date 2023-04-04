1adf7e20b69e57bb94e2716cec90315879f73db3
junior brandao <edilson_brandaojunior@hotmail.com>
2023-04-03 22:14:33 -0300

    RestTemplate

--- hr-payroll/src/main/java/com/eblj/hrpayroll/services/PaymentServiceImpl.java
+++ hr-payroll/src/main/java/com/eblj/hrpayroll/services/PaymentServiceImpl.java
@@ -1,16 +1,33 @@
 package com.eblj.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.eblj.hrpayroll.entities.Payment;
+import com.eblj.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
+	
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
 
 	@Override
-	public Payment getPayment(Long WorkId, Integer days) {
+	public Payment getPayment(Long workerId, Integer days) {
 		
-		return new Payment("Bob",200.00,days);
+		Map<String,String> uriVariables = new HashMap<>();
+		uriVariables.put("id",""+workerId);
+		
+		Worker worker = restTemplate.getForObject(workerHost+"/workes/{id}",Worker.class,uriVariables);	
+		return new Payment(worker.getName(),worker.getDailyIncome(),days);
 	}
 
    
