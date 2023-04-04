e41159f7cfd5cc94f32ebfecd9cf39811de32892
junior brandao <edilson_brandaojunior@hotmail.com>
2023-04-04 19:33:59 -0300

    Configuring Eureka server

--- hr-eureka-server/src/main/java/com/eblj/eurekaserver/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/com/eblj/eurekaserver/HrEurekaServerApplication.java
@@ -2,8 +2,11 @@ package com.eblj.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @SpringBootApplication
+@EnableEurekaServer
 public class HrEurekaServerApplication {
 
 	public static void main(String[] args) {
