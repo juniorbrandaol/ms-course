ba9faba472a0485dd0bf5d23f0096e543302a14f
junior brandao <edilson_brandaojunior@hotmail.com>
2023-04-05 12:43:03 -0300

    hr-worker as a config server client

--- hr-worker/src/main/java/com/eblj/hrworker/contollers/WorkerController.java
+++ hr-worker/src/main/java/com/eblj/hrworker/contollers/WorkerController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,18 +24,27 @@ public class WorkerController {
 	
 	private static Logger logger =  LoggerFactory.getLogger(WorkerController.class);
 	
+	@Value("${test.config}")
+	private String testeConfig;
+	
 	@Autowired  
 	private Environment env; // traz informações de todo o contexto da aplicação
 
 	@Autowired
 	private WorkerService service;
 	
+	@GetMapping(value="/configs")
+	@ResponseStatus(value = HttpStatus.OK)
+	public void getConfigs(){
+		logger.info("Config= "+testeConfig);
+	}
+	
+	
 	@GetMapping
 	@ResponseStatus(value = HttpStatus.OK)
 	public List<Worker> findAll(){
 		return service.listAll();
 	}
-	
 
 	@GetMapping(value="/{id}")
 	@ResponseStatus(value = HttpStatus.OK)
