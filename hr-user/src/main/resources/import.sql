03f5ba81e8015000848b0c1f0afd77de547c408d
junior brandao <edilson_brandaojunior@hotmail.com>
2023-04-05 20:48:01 -0300

    Data base seeding

---
+++ hr-user/src/main/resources/import.sql
@@ -0,0 +1,13 @@
+
+INSERT INTO tb_user (name, email, password) VALUES ('Nina Brown', 'nina@gmail.com', '$2a$10$NYFZ/8WaQ3Qb6FCs.00jce4nxX9w7AkgWVsQCG6oUwTAcZqP9Flqu');
+INSERT INTO tb_user (name, email, password) VALUES ('Leia Red', 'leia@gmail.com', '$2a$10$NYFZ/8WaQ3Qb6FCs.00jce4nxX9w7AkgWVsQCG6oUwTAcZqP9Flqu');
+INSERT INTO tb_user (name, email, password) VALUES ('Junior Brandão', 'jr@gmail.com', '$2a$10$8EG1A.vwU9YHcTsStAHFC.JAqrWlToyP6kRWvJm3T97mNP1lBtx3W');
+
+INSERT INTO tb_role (role_name) VALUES ('OPERATOR');
+INSERT INTO tb_role (role_name) VALUES ('ADMIN');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
