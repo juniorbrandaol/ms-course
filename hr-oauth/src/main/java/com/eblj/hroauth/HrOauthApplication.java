cb25575042f2f9630badefad066568ab632f2ee5
junior brandao <edilson_brandaojunior@hotmail.com>
2023-04-06 10:17:00 -0300

    Configuring hr-oauth project

--- hr-oauth/src/main/java/com/eblj/hroauth/HrOauthApplication.java
+++ hr-oauth/src/main/java/com/eblj/hroauth/HrOauthApplication.java
@@ -2,7 +2,10 @@ package com.eblj.hroauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
+@EnableEurekaClient
 @SpringBootApplication
 public class HrOauthApplication {
 
